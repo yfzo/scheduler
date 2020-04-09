dc13230ac912297162a0a1c513cac8509c0842ce
Yifei <yifeizhou.to@gmail.com>
2020-04-08 22:00:29 -0400

    Empty and existing appointments are displayed conditionally

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { Fragment } from "react";
 
-// import Header from "./Header"
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
-    return (
-        <article className="appointment"></article>
-    )
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+    </article>
+  );
 }
