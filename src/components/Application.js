2a51ca8b14e8339b059d54937c8b29784a1172a2
Yifei <yifeizhou.to@gmail.com>
2020-04-25 23:43:31 -0400

    Appointments are retrieved from API, appointments of selected day now shown

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,67 +6,32 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      },
-    },
-  },
-  {
-    id: 3,
-    time: "10am",
-    interview: {
-      student: "Yifei Zhou",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      },
-    },
-  },
-  {
-    id: 4,
-    time: "3pm",
-  },
-  {
-    id: 5,
-    time: "2:30pm",
-    interview: {
-      student: "Jeff Jun",
-      interviewer: {
-        id: 2,
-        name: "Tori Malcolm",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png",
-      },
-    },
-  },
-];
+import { getAppointmentsForDay } from "../helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
   });
 
-  const setDay = day => setState({ ...state, day });
-
-  const setDays = days => setState(prev => ({ ...prev, days }));
+  const setDay = (day) => setState({ ...state, day });
+  
+  const appointments = getAppointmentsForDay(state, state.day);
 
   useEffect(() => {
-    axios.get('http://localhost:8001/api/days')
-      .then((response) => setDays(response.data))
+    Promise.all([
+      Promise.resolve(axios.get("http://localhost:8001/api/days")),
+      Promise.resolve(axios.get("http://localhost:8001/api/appointments")),
+    ]).then((all) => {
+      const [days, appointments] = all;
+
+      setState((prev) => ({
+        ...prev,
+        days: days.data,
+        appointments: appointments.data,
+      }));
+    });
   }, []);
 
   return (
