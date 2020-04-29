bb86c130b49dd2cd080a3507b5ed8de9e6f9c334
Yifei <yifeizhou.to@gmail.com>
2020-04-29 18:09:49 -0400

    Added appointment transition to create new and back on cancel

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,16 +1,37 @@
 import React from "react";
+import useVisualMode from "../../hooks/useVisualMode";
 
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
 
 import "components/Appointment/styles.scss";
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+  function onAdd() {
+    transition(CREATE);
+  }
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={onAdd} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && <Form interviewers={[]} onCancel={back} />}
     </article>
   );
 }
